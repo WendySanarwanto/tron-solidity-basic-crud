4cb66bad2806a679585f2931fc255244f51b4a09
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-09-12 23:53:19 +0800

    Added additional parameters to interact with main net by using Tronweb

--- tronbox-config.js
+++ tronbox-config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const inventoryScAbi = require('./build/contracts/Inventory.json').abi;
 
+module.exports = {
+    "mainnet": {
+        "inventorySc": {
+            "address": process.env.MAIN_INVENTORY_SC,
+            "abi": inventoryScAbi
+        },
+        "nonAdminPks": [process.env.MAIN_NON_ADMIN_PK]
+    }
 };
