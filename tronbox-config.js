3826f8d4e494e67d88cf36d4920730e4d5dd1c54
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-09-14 23:15:32 +0800

    Added currentNet option and shasta config

--- tronbox-config.js
+++ tronbox-config.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
+
 const inventoryScAbi = require('./build/contracts/Inventory.json').abi;
 
 module.exports = {
+    "currentNet": process.env.CURRENT_NET,
     "mainnet": {
         "inventorySc": {
             "address": process.env.MAIN_INVENTORY_SC,
             "abi": inventoryScAbi
         },
         "nonAdminPks": [process.env.MAIN_NON_ADMIN_PK]
+    },
+    "shasta": {
+        "inventorySc": {
+            "address": process.env.SHASTA_INVENTORY_SC,
+            "abi": inventoryScAbi
+        },
+        "nonAdminPks": [process.env.SHASTA_NON_ADMIN_PK]
     }
 };
